bdb32fd99670182d9c92409556e9f5a5d9dc6573
an-bui-nguyen <phuong20102@gmail.com>
2021-12-29 11:51:13 +0700

    first commit

---
+++ part1/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name} and you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = "Anni"
+  const age = "20 tuổi r đụ má"
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name = {name} age = {age}/>
+    </div>
+  )
+}
+export default App
